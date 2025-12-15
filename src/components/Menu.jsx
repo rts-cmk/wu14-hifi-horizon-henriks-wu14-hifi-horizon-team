698f92276f84ab794884495e7c8805e83ee9900f
Henrik <han@rts.dk>
2025-12-15 13:39:50 +0100

    feat: add react-icons dependency and implement burger menu in Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,18 @@
 import { NavLink } from "react-router";
 import logo from '../assets/images/layout/logo.svg';
 import "./Menu.sass";
+import { IoIosMenu } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
 
 export default function Menu() {
 
     return (
         <nav className="menu">
+            <button type="button" className="menu__burger-button">
+                <IoIosMenu className="menu__burger-icon" />
+                <IoCloseOutline className="menu__close-icon" onClick={() => document.activeElement.blur()} />
+            </button>
+
             <ul className="menu__list">
                 <li className="menu__item">
                     <NavLink to='/' className="menu__link"><img className="menu__logo" src={logo} alt="Hi Fi Horizon logo" /></NavLink>
